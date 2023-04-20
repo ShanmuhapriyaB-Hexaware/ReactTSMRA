7f43e554c4c4577c7c3547b9307bf42ec2da6a2c
SanjithS-Hexaware <1000059279@hexaware.com>
2023-04-20 12:10:58 +0530

    Pushed Latest Boilerplate in react code sub generator

--- generator-swdp-react/generators/react-code/templates/src/App.tsx
+++ generator-swdp-react/generators/react-code/templates/src/App.tsx
@@ -1,4 +1,6 @@
-import React,{  useState } from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -6,7 +8,12 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Vite + React</h1>
+      <div>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -16,7 +23,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the React logo to learn more
       </p>
     </div>
   )
