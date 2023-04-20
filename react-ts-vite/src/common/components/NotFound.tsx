5ffcbfaa9dccc6305ce00c73ebb2063e4f5dbc98
ShanmuhapriyaB-Hexaware <ShanmuhapriyaB@hexaware.com>
2023-04-20 17:38:24 +0530

    Added routing for common components

---
+++ react-ts-vite/src/common/components/NotFound.tsx
@@ -0,0 +1,20 @@
+const NotFound = () => {
+  return (
+      <section>
+        <h3>Oops!</h3>
+        <h3>
+          The requested page cannot be found.
+        </h3>
+        <h3>
+          Sorry about that! Please visit our hompage to get where you need to go.
+        </h3>
+        <div>
+          <a href="/">
+            Back to Home!
+          </a>
+        </div>
+      </section>
+  );
+}
+
+export default NotFound;
--- react-ts-vite/src/configs/router/routes.config.tsx
+++ react-ts-vite/src/configs/router/routes.config.tsx
@@ -1,9 +1,18 @@
-import { RouteObject } from "react-router-dom"
+import { Navigate, RouteObject } from "react-router-dom"
 import homeRoutes from "../../main/home/home.routes"
+import  RedirectHere  from "../../common/components/RedirectHere"
+import NotAuthenticated from "../../common/components/NotAuthenticated"
+import UnAuthorized from "../../common/components/UnAuthorized"
+import NotFound from "../../common/components/NotFound"
 
 const routes = () => {
-    const all_routes : RouteObject[] = [
-        ...homeRoutes
+    const all_routes: RouteObject[] = [
+        ...homeRoutes,
+        { path: '/redirect-here', element: <RedirectHere /> },
+        { path: '/401', element: <NotAuthenticated /> },
+        { path: '/403', element: <UnAuthorized /> },
+        { path: '/404', element: <NotFound /> },
+        { path: '*', element: <Navigate to="/404" /> },
     ]
     return all_routes
 }
