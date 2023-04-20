29a5cd468498094b8c58ab42dcd1703f140c62e8
SanjithS-Hexaware <1000059279@hexaware.com>
2023-04-20 18:58:13 +0530

    Changes In Unauthorized Screen

---
+++ react-ts-vite/src/common/page/UnAuthorized.tsx
@@ -0,0 +1,20 @@
+const UnAuthorized = () => {
+  return (
+      <section>
+        <h3>403 Hold Up!</h3>
+        <h3>
+          You are unauthorized to view this page
+        </h3>
+        <h3>
+          Sorry about that! Please check the permission level to view this page.
+        </h3>
+        <div>
+          <a href="/">
+            Go to Home
+          </a>
+        </div>
+      </section>
+  );
+}
+
+export default UnAuthorized;
