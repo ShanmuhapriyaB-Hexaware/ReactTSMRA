eb5bc682aa382b9a4b03c095c6c044578ed9a919
ShanmuhapriyaB-Hexaware <ShanmuhapriyaB@hexaware.com>
2023-04-21 13:25:01 +0530

    Testing done: Jest framework

--- react-ts-vite/src/common/pages/Error.tsx
+++ react-ts-vite/src/common/pages/Error.tsx
@@ -1,7 +1,6 @@
 const ErrorFallback = () => {
     return (
         <div>
-            <img src="/assets/images/illustrations/designer.svg" alt="" />
             <h1>Something Went Wrong</h1>
             <h4>Contact Administrator</h4>
             <a href='/'>
