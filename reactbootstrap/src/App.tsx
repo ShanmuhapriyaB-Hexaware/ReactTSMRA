4a87ed14b7acb48efd691de1b6eb03916cbc6228
SanjithS-Hexaware <1000059279@hexaware.com>
2023-05-22 21:14:33 +0530

    Generated React TS Vite with Bootstrap

---
+++ reactbootstrap/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, useRoutes } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary'
+import routes from './configs/router/routes.config';
+import ErrorFallback from './common/pages/Error'
+import AppContext from './AppContext';
+
+const GlobalRoutes = () => useRoutes(routes())
+
+function App() {
+
+  return (
+    <AppContext.Provider value={{ routes }}>
+      <Router>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <GlobalRoutes />
+        </ErrorBoundary>
+      </Router>
+    </AppContext.Provider>
+  )
+}
+
+export default App
---
+++ reactbootstrap/src/common/pages/RedirectHere.tsx
@@ -0,0 +1,12 @@
+const RedirectHere = () => {
+    return (
+        <div className="snippet">
+        <h2>Redirecting</h2>
+        <div className="stage">
+          <div className="dot-pulse"></div>
+        </div>
+      </div>
+    )
+}
+
+export default RedirectHere
